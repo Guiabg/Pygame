135ccf094260b40a0c538c926b1e4b9770b04d43
Guiabg <guilhermebranco.ipms@gmail.com>
2025-09-21 16:14:13 -0300

    condições IF adicionadas para melhor separação de estados de jogo e tela de game over

--- Pygame-main/Game/sambanalua.py
+++ Pygame-main/Game/sambanalua.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode((1300,750))
 pygame.display.set_caption('Samba na Lua')
 clock = pygame.time.Clock()
 fonte_teste = pygame.font.Font('Pygame-main\\font\\Pixeltype.ttf', 50)
+jogo_ativo = True
 
 
 #Imagens e textos usados
@@ -40,42 +41,55 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if astro_rect.collidepoint(event.pos) and astro_rect.bottom >=550:
-                astro_grav = -17
-        '''if event.type == pygame.MOUSEBUTTONDOWN:
-            print('Mouse down')
-        if event.type == pygame.MOUSEBUTTONUP:
-            print('Mouse up')'''
-        #Controles
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and astro_rect.bottom >=550:
-                astro_grav = -17
+        if jogo_ativo:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if astro_rect.collidepoint(event.pos) and astro_rect.bottom >=550:
+                    astro_grav = -17
+            '''if event.type == pygame.MOUSEBUTTONDOWN:
+                print('Mouse down')
+            if event.type == pygame.MOUSEBUTTONUP:
+                print('Mouse up')'''
+            #Controles
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and astro_rect.bottom >=550:
+                    astro_grav = -17
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                jogo_ativo = True
+                snail_rect.left = 1000
+    if jogo_ativo:
 
-    #Camadas
-    screen.blit(space_big,(0,0))
-    screen.blit(ground_big,(0,550))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
-    #pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50,200,100,100)) - Para criar círculos ou elípses.
-    #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(), 10) - para uma linha apontar para a posição do mouse
-    
-    screen.blit(text_score_surf,(score_rect))
-    """snail_x_pos-=4
-    if snail_x_pos <-100: snail_x_pos = 800"""
-    snail_rect.x -= 4
-    if snail_rect.right <=0: snail_rect.left = 800
+        #Camadas
+        screen.blit(space_big,(0,0))
+        screen.blit(ground_big,(0,550))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+        #pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50,200,100,100)) - Para criar círculos ou elípses.
+        #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(), 10) - para uma linha apontar para a posição do mouse
+        
+        screen.blit(text_score_surf,(score_rect))
+        """snail_x_pos-=4
+        if snail_x_pos <-100: snail_x_pos = 800"""
+        snail_rect.x -= 4
+        if snail_rect.right <=0: snail_rect.left = 800
 
-    screen.blit(snail_big,snail_rect)
-    # print(player_rectangle.left) para ver a posição da linha de certo lado do retângulo.
-    # player_rectangle.left +=1 irá mover o personagem para a esquerda da tela.
+        screen.blit(snail_big,snail_rect)
+        # print(player_rectangle.left) para ver a posição da linha de certo lado do retângulo.
+        # player_rectangle.left +=1 irá mover o personagem para a esquerda da tela.
 
-    #Astronauta
-    astro_grav +=0.5
-    astro_rect.y += astro_grav
-    if astro_rect.bottom >=550:
-        astro_rect.bottom = 550
-    screen.blit(astro_big, astro_rect)
+        #Astronauta
+        astro_grav +=0.5
+        astro_rect.y += astro_grav
+        if astro_rect.bottom >=550:
+            astro_rect.bottom = 550
+        screen.blit(astro_big, astro_rect)
+        if snail_rect.colliderect(astro_rect):
+            jogo_ativo = False
+
+    #Menu
+    else:
+        screen.fill('Yellow')
+
 
     #Teste de colisão
     # print(player_rectangle.colliderect(snail_rectangle)) para checar colisão entre dois objetos.
