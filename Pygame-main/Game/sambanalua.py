b38b6fccf0f4ba5a80f257dd05b685d501118ea5
Guiabg <guilhermebranco.ipms@gmail.com>
2025-09-21 16:33:56 -0300

    Pontuação adicionada

--- Pygame-main/Game/sambanalua.py
+++ Pygame-main/Game/sambanalua.py
@@ -2,6 +2,13 @@
 import pygame
 
 from sys import exit
+
+def display_score():
+    current_time = int (pygame.time.get_ticks() / 1000) - start_time
+    score_surf = fonte_teste.render(f'Tempo: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+
 #Janela
 pygame.init()
 screen = pygame.display.set_mode((1300,750))
@@ -9,6 +16,7 @@ pygame.display.set_caption('Samba na Lua')
 clock = pygame.time.Clock()
 fonte_teste = pygame.font.Font('Pygame-main\\font\\Pixeltype.ttf', 50)
 jogo_ativo = True
+start_time = 0
 
 
 #Imagens e textos usados
@@ -17,7 +25,7 @@ jogo_ativo = True
 space_surf = pygame.image.load('Pygame-main\sprites\space.png').convert()
 ground_surf = pygame.image.load('Pygame-main\graphics\ground.png').convert()
 snail_surf = pygame.image.load('Pygame-main\graphics\snail\snail1.png').convert_alpha()
-text_score_surf = fonte_teste.render('Teste.', False, (64,64,64))
+#text_score_surf = fonte_teste.render('Teste.', False, (64,64,64))
 astro_surf = pygame.image.load('Pygame-main\\sprites\\spr_1.png').convert_alpha()
 
 #Imagens rescalionadas
@@ -28,7 +36,7 @@ snail_big = pygame.transform.scale(snail_surf, (100,60)).convert_alpha()
 
 #Posições e colisões
 snail_x_pos = 600
-score_rect = text_score_surf.get_rect(center = (650, 50))
+#score_rect = text_score_surf.get_rect(center = (650, 50))
 snail_rect = snail_big.get_rect(bottomright = (100,550))
 astro_rect = astro_big.get_rect(midbottom = (300,550))
 
@@ -57,17 +65,20 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 jogo_ativo = True
                 snail_rect.left = 1000
+                start_time = int(pygame.time.get_ticks() / 1000)
+
     if jogo_ativo:
 
         #Camadas
         screen.blit(space_big,(0,0))
         screen.blit(ground_big,(0,550))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+        #pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        #pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
         #pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50,200,100,100)) - Para criar círculos ou elípses.
         #pygame.draw.line(screen, 'Gold', (0,0), pygame.mouse.get_pos(), 10) - para uma linha apontar para a posição do mouse
         
-        screen.blit(text_score_surf,(score_rect))
+        #screen.blit(text_score_surf,(score_rect))
+        display_score()
         """snail_x_pos-=4
         if snail_x_pos <-100: snail_x_pos = 800"""
         snail_rect.x -= 4
